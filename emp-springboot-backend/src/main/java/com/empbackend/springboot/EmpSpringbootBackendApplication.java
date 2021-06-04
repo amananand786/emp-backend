a1f41f216c47d7bad56c978741f0c830b765ff5b
11436 <11436@AQKT075-5410.AQ.Local>
2021-06-04 15:54:45 +0530

    first commit 4 june emp backend configured with oracle database

---
+++ emp-springboot-backend/src/main/java/com/empbackend/springboot/EmpSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.empbackend.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ emp-springboot-backend/src/test/java/com/empbackend/springboot/EmpSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empbackend.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
